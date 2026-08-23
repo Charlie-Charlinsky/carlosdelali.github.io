6ed6cfb5159cc20ade39c75e9dfafcba611ba3aa
Charlie-Charlinsky <76741551+Charlie-Charlinsky@users.noreply.github.com>
2026-08-23 17:12:42 +0200

    Polish CV layout and chronology

--- js/pages/cv.js
+++ js/pages/cv.js
@@ -2,6 +2,12 @@ import { loadJson, loadSemanticFragment } from "../core/loaders.js";
 import { createElement, setPageTitle } from "../core/dom.js";
 import { detailUrl } from "../core/routes.js";
 
+const LUDOGRAPHY_STUDIO_ORDER = new Map([
+    ["ea-sports", 0],
+    ["tws-inventors-of-play", 1],
+    ["genera-games", 2]
+]);
+
 export async function render({ language, target }) {
     const [fragment, gamesRegistry, ludography] = await Promise.all([
         loadSemanticFragment(`content/cv/${language}.html`),
@@ -13,8 +19,13 @@ export async function render({ language, target }) {
     const ludographySection = article.querySelector("#ludography");
     const gameMap = new Map(gamesRegistry.games.map((game) => [game.id, game]));
     const catalogue = createElement("div", { className: "ludography" });
+    const orderedStudios = [...ludography.studios].sort((first, second) => {
+        const firstOrder = LUDOGRAPHY_STUDIO_ORDER.get(first.id) ?? Number.MAX_SAFE_INTEGER;
+        const secondOrder = LUDOGRAPHY_STUDIO_ORDER.get(second.id) ?? Number.MAX_SAFE_INTEGER;
+        return firstOrder - secondOrder;
+    });
 
-    ludography.studios.forEach((studio) => {
+    orderedStudios.forEach((studio) => {
         const group = createElement("section", { className: "ludography-group" });
         group.append(createElement("h3", { text: studio.name }));
         const list = createElement("ol", { className: "ludography-list" });
